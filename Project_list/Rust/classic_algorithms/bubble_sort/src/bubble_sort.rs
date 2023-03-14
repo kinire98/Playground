a302d29442539a630164b4374bc6a309cb21e9a4
kinire98 <93119213+kinire98@users.noreply.github.com>
2023-03-14 20:31:15 +0100

    some new things

--- Project_list/Rust/classic_algorithms/bubble_sort/src/bubble_sort.rs
+++ Project_list/Rust/classic_algorithms/bubble_sort/src/bubble_sort.rs
@@ -1,10 +1,13 @@
 pub fn bubble_sort(mut arr: Vec<i32>) -> Vec<i32> {
     for i in (0..arr.len()).rev() {
+        let mut swapped = false;
         for j in 0..i + 1 {
             if j == arr.len() - 1 {continue;}
             if arr[j] > arr[j + 1] {
                 arr.swap(j, j + 1);
+                swapped = true;
             }
+            if swapped == false {break;}
         }
     }
     arr
